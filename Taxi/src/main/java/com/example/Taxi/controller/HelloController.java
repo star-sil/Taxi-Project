a2890ae3c831473f82ad1655e84076fe3f72785c
star-sil <kimse9811@naver.com>
2021-12-24 20:48:21 +0900

    init⚡️

---
+++ Taxi/src/main/java/com/example/Taxi/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.Taxi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "김성은 신영한 구현우 Taxi 프로젝트 시작 \n" + new Date();
+    }
+}
---
+++ Taxi/src/test/java/com/example/Taxi/TaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
