9c60a7eb7926db6a105669474e588fcc9037500b
star-sil <kimse9811@naver.com>
2022-01-07 21:52:53 +0900

    api 방식 변경

--- Taxi/src/main/java/com/example/Taxi/controller/HelloController.java
+++ Taxi/src/main/java/com/example/Taxi/controller/HelloController.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "김성은 신영한 구현우 Taxi 프로젝트 시작 \n" + new Date();
     }
