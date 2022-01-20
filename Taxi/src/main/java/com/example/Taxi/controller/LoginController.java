39a8aca3c6ce42dfabe106e9ba2e42a983120323
star-sil <kimse9811@naver.com>
2022-01-20 10:55:10 +0900

    로그 추가

--- Taxi/src/main/java/com/example/Taxi/controller/LoginController.java
+++ Taxi/src/main/java/com/example/Taxi/controller/LoginController.java
@@ -1,16 +1,19 @@
 package com.example.Taxi.controller;
 
 import com.example.Taxi.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
+@Slf4j
 @Controller
 public class LoginController {
     @ResponseBody
     @RequestMapping(value = "/start", method = RequestMethod.POST)
     public String start(@RequestBody Member member){
+        log.info(member.getClass().toString());
         if(member.getPassword() == 1234)
             return "success";
         return "fail";
