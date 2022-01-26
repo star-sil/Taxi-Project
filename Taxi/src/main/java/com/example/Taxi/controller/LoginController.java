9b766f3b69c1c7c2b6e631f3cf8c63792ff51e77
star-sil <kimse9811@naver.com>
2022-01-26 11:57:21 +0900

    필요없는 라이브러리 제거

--- Taxi/src/main/java/com/example/Taxi/controller/LoginController.java
+++ Taxi/src/main/java/com/example/Taxi/controller/LoginController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 
 @Slf4j
 @Controller
