0b47d1241aac9a506f5499eaf436708f48a509aa
GuHyeonWoo <ghw9174@gmail.com>
2022-03-01 09:43:00 +0900

    front 코드 추가

--- taxifront/src/RoomModal.js
+++ taxifront/src/RoomModal.js
@@ -1,8 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './RoomModal.css'
 import { Modal, Button, FormControl } from 'react-bootstrap';
 
 function RoomModal(props) {
+    const [inputDes , setDes] = useState('');
+    const handlerDes = (e) => { setDes(e.target.value)};
+    const [inputHour , setHour] = useState(12);
+    const handlerHour = (e) => { setHour(1 * e.target.value)};
+    const [inputMin , setMin] = useState(0);
+    const handlerMin = (e) => { setMin(1 * e.target.value)};
+
+    useEffect(()=> {
+        props.parentCallback(inputDes, inputHour, inputMin);
+    }, );
+
     return (
         <Modal id = "Modal"
             {...props}
@@ -24,35 +35,44 @@ function RoomModal(props) {
                         placeholder="EX) 구미역"
                         aria-label="Username"
                         aria-describedby="basic-addon1"
+                        onChange={handlerDes}
                         />
                     </div>    
                 </div>
+                <p>
+                    되도록이면, 정확한 목적지를 입력하여 주세요.
+                </p>
                 <hr></hr>
                 <div id = 'modal'>
                     <h3 id='modaltitle'>☑️ 출발 시간</h3>
                     <div id = 'input'>
                         <FormControl id = 'hour'
+                        type='number'
                         placeholder="12"
                         aria-label="Username"
                         aria-describedby="basic-addon1"
+                        onChange={handlerHour}
                         />
                         <h4>시</h4>
                         <FormControl id = 'min'
+                        type='number'
                         placeholder="00"
                         aria-label="Username"
                         aria-describedby="basic-addon1"
+                        onChange={handlerMin}
                         />
                         <h4>분</h4>
                     </div>
                 </div>
+                <p>
+                    시간은 0 ~ 23, 분은 0 ~ 59 범위 내로 입력하여 주세요.
+                </p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant='secondary' onClick={props.onHide}>취소</Button>
                 <Button variant='primary' onClick={props.saveInfo} >확인</Button>
             </Modal.Footer>
-        </Modal>
-
-        
+        </Modal>     
     )
 }
 
